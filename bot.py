35759f5c998af36593e8a77d137cf2e041e31a8f
maximmim <93773088+maximmim@users.noreply.github.com>
2022-06-29 14:21:44 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,80 +6,68 @@ from aiogram.utils import executor
 from aiogram.utils.emoji import emojize
 
 import re
-import os 
-
+import os
+import random
 from time import sleep
-
+import time
 
 
 import yt_download as yt
 import config 
-import time 
-import random
 
-'''***********************************************Фунции***************************************************************'''
 
 def random_id():
     return random.randint(0, 1000000)
-def random_dw ():
-    return random.choice(y)
-'''***********************************************Конвази*************************************************************'''
 
-
-
-
-
-bot = Bot(token="5312847705:AAE0ii_TUhEeuNPRV52iiFmB0bsEInhANt4")
+bot = Bot(config.TOKEN)
+URL = "https://painmo.herokuapp.com/"
 dp = Dispatcher(bot)
 
+b1 = KeyboardButton('Помощь')
+b2 = KeyboardButton('Скачять')
 
+kb_client = ReplyKeyboardMarkup()
+kb_client.add(b1).add(b2)
 
-'''**************************************Админская часть****************************************************'''
-
-
-
-
-
-
-
-
-'''*****************************Клиенская часть***************************************'''
-
-
-
-
-
+   
 @dp.message_handler(commands=['start'])
-async def commands_start(message: types.message):
-    await message.reply("Привіт я багато функціональний телеграм бот\n Та звати мене Паймон")
+async def process_help_command(message: types.Message):
+    await message.reply("Привіт!\n Я Паймон і допоможу завантажити відео з будь-якого сайту якщо потрібна допомога пиши /help")
+    await message.delete()
 
 
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply(config.STARTEXT)
+    await message.reply("Надішліть мені посилання на відео \n і я допоможу завантажити його")
+    await message.delete()
 @dp.message_handler(content_types=['text'])
 async def echo_download_message(message: types.Message):
-
-       
-       try:
-           echo_download=yt.Downloader(message.text)
-           await message.reply("Побачила, починаю закачку...")
-           videonote = open(echo_download.download_video(), 'rb')
-       except:
-           await message.reply("На жаль, сталася помилка... Перевірте правильність посилання")
-           print('Error :(')
-           return
-           
-       try:
-           await message.reply("Готово, видео зкачено на сервер.\nВідправляю...",)
-           await bot.send_document(message.from_user.id, videonote)
-       except:
-           await bot.send_message(message.from_user.id, "На жаль, сталася помилка...")
-       finally:
-           videonote.close()
-
-
-  
+    try:
+        
+        echo_download=yt.Downloader(message.text)
+        await message.reply("Побачила, починаю закачку...")        
+        videonote = open(echo_download.download_video(), 'rb')
+     
+    except:
+        await message.reply("На жаль, сталася помилка... Перевірте правильність посилання")
+        print('Error :(')
+        return
+    await message.reply("Готово, видео зкачено на сервер.\nВідправляю...",)
+    try:
+        await bot.send_document(message.from_user.id, videonote)
+    except:
+        await bot.send_message(message.from_user.id, "На жаль, сталася помилка...")
+    finally:
+        videonote.close()
 
 
 
 
-print("Оно живое !!!")
-executor.start_polling(dp, skip_updates=True)
+
+
+
+
+print("Оно живое !")
+executor.start_polling(dp)
--- config.py
+++ config.py
@@ -1,2 +1,4 @@
+TOKEN="5312847705:AAE0ii_TUhEeuNPRV52iiFmB0bsEInhANt4"
 download_folder='user_videos'
 NAME = 'Paimon'
+STARTEXT = "Привіт я багато функціональний телеграм бот. \n Та звати мене Паймон"
